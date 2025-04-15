8796d6fc6098ae147f526a4f8ae8b374e5369540
Kelechi Ukpabi <kuolingou@gmail.com>
2025-04-15 19:02:04 -0400

    add google analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-CV8438E31K"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CV8438E31K');
+        `}
+      </Script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {children}
