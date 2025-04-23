2b5334c339ae4c82e9b895ebbb26f7ed342ce648
Kelechi Ukpabi <kuolingou@gmail.com>
2025-04-22 21:05:06 -0400

    add health controller

---
+++ server/src/main/java/com/main/server/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.main.server.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+  @GetMapping("/health")
+  public ResponseEntity<String> health() {
+    return ResponseEntity.ok("OK");
+  }
+}
